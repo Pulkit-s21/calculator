6594f126b80b175899f70b264ca89624be62d56b
Harshit Saxena <18752758+Harshit-Saxena@users.noreply.github.com>
2022-02-16 22:20:41 +0530

    Invalid mathematical operation check '2++2'

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const btns = document.querySelectorAll(".btn");
 const screen = document.querySelector(".screen");
 const equalBtn = document.querySelector(".btn-equal");
 const clearBtn = document.querySelector(".btn-clear");
+const opp = document.querySelector(".operator");
+
+//get value of operator for invalid mathematical operation check
+const oppVal = opp.value();
 
 btns.forEach((btn) => {
     btn.addEventListener("click", () => {
@@ -13,8 +17,10 @@ btns.forEach((btn) => {
 equalBtn.addEventListener("click", () => {
     if (screen.value === '') {
         alert("Input is empty");
-    }
-    else {
+        console.log(oppVal);
+    } else if(screen.value === ''+oppVal+oppVal+''){ //invalid input 'more that one operator between two digits'
+        alert("Invalid Mathemicatical Operation");
+    }else {
         let result = eval(screen.value);
         // added this to that the result is never more than 3 decimal digits
         screen.value = parseFloat(result).toFixed(3);
